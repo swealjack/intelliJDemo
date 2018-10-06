f170ec4ac4eb54a4ef505aab25c4f699ad72e5a2
swealjack <umair_msn@hotmail.com>
2018-10-06 06:50:08 +0500

    updated by umair. 2nd version

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.print("Umair's  GIT push!!");
-        System.out.println("COMMIT NEW")
+        System.out.println("COMMIT NEW");
     }
 }
